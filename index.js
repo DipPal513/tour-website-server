2f4d318d38e0af42185dafdea7000ef6b8207619
DipPal513 <dip.pal.513@gmail.com>
2021-10-31 15:05:00 +0600

    's'

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzftm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("tourServices");
+    const servicesDB = database.collection("services");
+    const tourUser = database.collection("tourUser");
+    // POST API
+    app.post("/addService", async (req, res) => {
+      const data = req.body;
+      const result = await servicesDB.insertOne(data);
+      res.json(result);
+    });
+
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesDB.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // my orders
+
+    app.get("/myOrders",async (req,res) => {
+      const cursor = tourUser.find({});
+      const orders= await cursor.toArray();
+      res.json(orders);
+     
+    })
+    app.get("/myOrders/:email", async (req, res) => {
+
+      const myOrder = await tourUser
+        .find({
+          email: req.params.email,
+        })
+        .toArray();
+      res.send(myOrder);
+     
+    });
+
+    // GET API WITH ID
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesDB.findOne(query);
+      res.json(service);
+    });
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await tourUser.deleteOne({
+        _id: ObjectId(id),
+      });
+      console.log('deleted.fired')
+      res.send(result);
+    });
+  } finally {
+    // client.close();
+  }
+}
+
+run().catch((err) => {
+  console.dir;
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi from node js....");
+});
+app.listen(port, () => {
+  console.log("server start at port ", port);
+});
